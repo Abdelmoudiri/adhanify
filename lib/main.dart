b5f3583ae9ba0149d6d5a3c1604e8d66e03531b6
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-12-11 22:20:27 +0100

    Refactor main.dart to remove unused imports and set HomeScreen as the main interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,18 @@
-import 'package:adhanify/features/models/prayer_times.dart';
-import 'package:adhanify/features/services/prayer_api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:adhanify/features/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? Key}) : super(key: Key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Adhanify'),
-        ),
-        body: FutureBuilder(
-          future: PrayerApiService().fetchPrayerTimes(33.5731, -7.5898), 
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting){
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-
-            } else{
-              final prayerTimes = snapshot.data!;
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Fajr: ${prayerTimes.fajr}'),
-                  Text('Dhuhr: ${prayerTimes.dhuhr}'),
-                  Text('Asr: ${prayerTimes.asr}'),
-                  Text('Maghrib: ${prayerTimes.maghrib}'),
-                  Text('Isha: ${prayerTimes.isha}'),
-                ],
-              );
-            }
-          }
-      ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
     );
   }
-
 }
